c1531f50a2cc50393546d6ee476aea1b368fa4da
Angger Binuko <angger.binuko@gmail.com>
2023-06-23 11:02:47 +0700

    tambahan file Pertemuan 4

--- navbar.php
+++ navbar.php
@@ -47,7 +47,7 @@
     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
     <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
-</div>
+
 </div>
 </nav>
 <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
