cefa153ffe05b26725438abd20f040000e4d46bb
Angger Binuko <angger.binuko@gmail.com>
2023-07-03 15:54:05 +0700

    project 2 order form

--- project2/app.js
+++ project2/app.js
@@ -8,6 +8,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event){
 
     if(email == 'admin@example.com' && password == 'admin'){
         alert('Anda Berhasil Login!');
+        location.replace('order.html');
     } else {
         var errorMessage = document.createElement('div');
         errorMessage.textContent = 'Email atau Password salah'
@@ -17,3 +18,21 @@ document.getElementById('loginForm').addEventListener('submit', function(event){
     }
     
 });
+
+function calculateCost(){
+    var menuItem = document.getElementsByName('menu');
+    var totalCost = 0;
+    
+    for(var i=0; i < menuItem.length; i++){
+        if(menuItem[i].checked){
+            totalCost += parseInt(menuItem[i].value);
+            menuItem[i].parentNode.style.backgroundColor = 'green';
+            menuItem[i].parentNode.style.color = 'white';
+        } else {
+            menuItem[i].parentNode.style.backgroundColor = 'white';
+            menuItem[i].parentNode.style.color = 'initial';
+        }
+    }
+
+    document.getElementById('totalCost').innerHTML = '<span class="left">Total:</span><span class="right">Rp '+totalCost.toLocaleString()+'</span>'    
+}
